641c17291b235e979d38d6669ccf632430c484be
amaliekea <amje0001@stud.kea.dk>
2024-11-28 15:58:27 +0100

    add og delete virker

--- src/main/java/org/example/eksamenkea/controller/TaskController.java
+++ src/main/java/org/example/eksamenkea/controller/TaskController.java
@@ -3,9 +3,8 @@ package org.example.eksamenkea.controller;
 import jakarta.servlet.http.HttpSession;
 import org.example.eksamenkea.model.*;
 import org.example.eksamenkea.service.Errorhandling;
+import org.example.eksamenkea.service.SubprojectService;
 import org.example.eksamenkea.service.TaskService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,18 +25,15 @@ public class TaskController {
     @GetMapping("/add-task")
     public String addTask(@RequestParam("subprojectName") String subprojectName, HttpSession session, Model model) throws Errorhandling {
         Task task = new Task();
-        Role userRole = (Role) session.getAttribute("userRole");  // Henter "userrole" fra sessionen.
         Employee employee = (Employee) session.getAttribute("employee");  // Henter "user" fra sessionen.
         int subprojectId = taskService.getSubprojectIdBySubprojectName(subprojectName);
 
-        if (userRole == Role.PROJECTLEADER) {
             model.addAttribute("task", task);
             model.addAttribute("employeeId", employee.getEmployee_id());
             model.addAttribute("subprojectId", subprojectId);
             model.addAttribute("subprojectName", subprojectName);
             return "add-task";
-        }
-        throw new Errorhandling("cant add task");
+
     }
 
     @PostMapping("/task-added") //Amalie
@@ -48,7 +44,7 @@ public class TaskController {
 
     @GetMapping("/project-leader-tasks")
     public String getTaskBySubprojectName(@RequestParam("subprojectName") String subprojectName, HttpSession session, Model model) throws Errorhandling {
-        try {
+
             // Hent subproject ID baseret på subproject name
             int subprojectId = taskService.getSubprojectIdBySubprojectName(subprojectName);
 
@@ -60,10 +56,6 @@ public class TaskController {
             model.addAttribute("subprojectName", subprojectName);
 
             return "project-leader-task-overview";
-        } catch (Errorhandling e) {
-            model.addAttribute("errorMessage", e.getMessage());
-            return "error/error";
-        }
     }
 
 }
