33498a03e5a42b7a0d05519489ab05b8718b57fe
malthe-kea <mati0002@stud.kea.dk>
2024-12-04 13:41:54 +0100

    Back-knap tilføjet.

--- src/main/java/org/example/eksamenkea/model/Employee.java
+++ src/main/java/org/example/eksamenkea/model/Employee.java
@@ -1,28 +1,28 @@
 package org.example.eksamenkea.model;
 
 public class Employee {
-    private int employee_id;
+    private int employeeId;
     private String email; // fungerer som username
     private String password;
     private Role role; // Enum type
-    private int employee_rate;
-    private int max_hours; // Maksimale timer
+    private int employeeRate;
+    private int maxHours; // Maksimale timer
 
-    public Employee(int employee_id, String email, String password, Role role, int employee_rate, int max_hours) {
-        this.employee_id = employee_id;
+    public Employee(int employeeId, String email, String password, Role role, int employeeRate, int maxHours) {
+        this.employeeId = employeeId;
         this.email = email;
         this.password = password;
         this.role = role;
-        this.employee_rate = employee_rate;
-        this.max_hours = max_hours;
+        this.employeeRate = employeeRate;
+        this.maxHours = maxHours;
     }
 
-    public int getEmployee_id() {
-        return employee_id;
+    public int getEmployeeId() {
+        return employeeId;
     }
 
-    public void setEmployee_id(int employee_id) {
-        this.employee_id = employee_id;
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
     }
 
     public String getEmail() {
@@ -49,31 +49,31 @@ public class Employee {
         this.role = role;
     }
 
-    public int getEmployee_rate() {
-        return employee_rate;
+    public int getEmployeeRate() {
+        return employeeRate;
     }
 
-    public void setEmployee_rate(int employee_rate) {
-        this.employee_rate = employee_rate;
+    public void setEmployeeRate(int employeeRate) {
+        this.employeeRate = employeeRate;
     }
 
-    public int getMax_hours() {
-        return max_hours;
+    public int getMaxHours() {
+        return maxHours;
     }
 
-    public void setMax_hours(int max_hours) {
-        this.max_hours = max_hours;
+    public void setMaxHours(int maxHours) {
+        this.maxHours = maxHours;
     }
 
     @Override
     public String toString() {
         return "Employee{" +
-                "employee_id=" + employee_id +
+                "employee_id=" + employeeId +
                 ", email='" + email + '\'' +
                 ", password='" + password + '\'' +
                 ", role=" + role +
-                ", employee_rate=" + employee_rate +
-                ", max_hours=" + max_hours +
+                ", employee_rate=" + employeeRate +
+                ", max_hours=" + maxHours +
                 '}';
     }
 }
