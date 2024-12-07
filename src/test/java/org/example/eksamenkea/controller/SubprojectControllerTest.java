f4396eb0e306709239ae845ce79690fb9128a3b5
“zuunun” <zuun0001@stud.kea.dk>
2024-12-07 16:03:27 +0100

    EmployeeControllerTest

---
+++ src/test/java/org/example/eksamenkea/controller/SubprojectControllerTest.java
@@ -0,0 +1,25 @@
+package org.example.eksamenkea.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SubprojectControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getSubprojectToEdit() {
+    }
+
+    @Test
+    void editSubproject() {
+    }
+
+    @Test
+    void showProjectLeaderSubprojectOverview() {
+    }
+}
