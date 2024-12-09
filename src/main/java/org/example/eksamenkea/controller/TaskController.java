035f870d503ffc31017356e1a2ec22fcc3c4e9b5
amaliekea <amje0001@stud.kea.dk>
2024-12-09 18:57:47 +0100

    fjernet max hours

--- src/main/java/org/example/eksamenkea/controller/TaskController.java
+++ src/main/java/org/example/eksamenkea/controller/TaskController.java
@@ -60,6 +60,7 @@ public class TaskController {
 
         return "project-leader-task-overview";
     }
+
     @GetMapping("/worker-overview") //Malthe
     public String showWorkerOverview(HttpSession session, Model model) throws Errorhandling {
         Employee employee = (Employee) session.getAttribute("employee");
@@ -88,25 +89,13 @@ public class TaskController {
                                        @RequestParam("taskId") int taskId, @RequestParam("employeeEmail") String employeeEmail, Model model, HttpSession session) throws Errorhandling {
         Employee employee = employeeService.getEmployeeByEmail(employeeEmail);
         employee.setEmployeeId(employee.getEmployeeId());
-        Task task1 = taskService.getTaskByTaskId(taskId);
-        int taskHours = task1.getEstimatedHours();
-        List<Task> tasklist = taskService.getTasklistByEmployeeId(employee.getEmployeeId());
-        int totalTaskHours = 0;
-
-        for (Task task : tasklist) {
-            totalTaskHours += task.getEstimatedHours();
-        }
 
         model.addAttribute("subprojectId", subprojectId);
         model.addAttribute("taskId", taskId);
-        model.addAttribute("totalTaskHours", totalTaskHours);
         model.addAttribute("employee", employee);
 
-        if ((totalTaskHours + taskHours) <= employee.getMaxHours()) {
-            taskService.assignWorkerToTask(taskId, employee.getEmployeeId());
-        } else {
-            return "error/error-exceed-max-hours-for-worker";
-        }
+        taskService.assignWorkerToTask(taskId, employee.getEmployeeId());
+
         return "redirect:/project-leader-tasks?subprojectId=" + subprojectId;
 
     }
