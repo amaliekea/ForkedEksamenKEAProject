aef70ca782472558f0f0f1ef37b7d5b8d332c16f
malthe-kea <mati0002@stud.kea.dk>
2024-12-09 19:42:16 +0100

    Fikset log out knappen
    Fikset alle overskrifter med hvid boks bagved

--- src/test/java/org/example/eksamenkea/repository/ProjectRepositoryTest.java
+++ src/test/java/org/example/eksamenkea/repository/ProjectRepositoryTest.java
@@ -2,16 +2,13 @@ package org.example.eksamenkea.repository;
 
 import org.example.eksamenkea.model.Project;
 import org.example.eksamenkea.service.Errorhandling;
-import org.example.eksamenkea.service.ProjectService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -19,15 +16,17 @@ import static org.mockito.Mockito.verify;
 @SpringBootTest
 @AutoConfigureMockMvc
 @ExtendWith(MockitoExtension.class) //Den gør, at Mockito kan håndtere dine @Mock og @InjectMocks annotationer automatisk uden behov for yderligere opsætning
+
 class ProjectRepositoryTest {
     @Autowired
     private ProjectRepository projectRepository;
+
+    Project project = new Project(1, "testproject", 2000, "testdescription", 1, 100);
 
 
     @Test
     void addProject() throws Errorhandling {
         // Arrange
-        Project project = new Project(1, "testproject", 2000, "testdescription", 1, 100);
 
         // Act
         projectRepository.addProject(project);
@@ -51,15 +50,15 @@ class ProjectRepositoryTest {
     @Test
     void archiveProject() throws Errorhandling {
         int projectId = 1;
-        Project objTest = projectRepository.getProjectFromProjectId(projectId);
+        Project testProject1 = projectRepository.getProjectFromProjectId(projectId);
 
-        assertTrue(objTest.getProjectName().equals("testproject"));
+        assertTrue(testProject1.getProjectName().equals("testproject"));
 
         projectRepository.archiveProject(projectId);
 
-        Project objTest2 = projectRepository.getProjectFromProjectId(projectId);
+        Project testProject2 = projectRepository.getProjectFromProjectId(projectId);
 
-        assertNull(objTest2);
+        assertNull(testProject2);
     }
 
     @Test
