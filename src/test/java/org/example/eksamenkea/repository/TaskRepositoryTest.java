aa4d7f8eb60cb2fb90fb16601d3ef9a4b7a7e1c1
malthe-kea <mati0002@stud.kea.dk>
2024-12-11 13:42:54 +0100

    Opdateret baggrund.

---
+++ src/test/java/org/example/eksamenkea/repository/TaskRepositoryTest.java
@@ -0,0 +1,20 @@
+package org.example.eksamenkea.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskRepositoryTest {
+
+    @Test
+    void createTask() {
+    }
+
+    @Test
+    void getTaskByTaskId() {
+    }
+
+    @Test
+    void assignWorkerToTask() {
+    }
+}
